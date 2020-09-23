fe2ea5f423cf18822b369e13b2bb77590c3cf605
Joo.ssung <tjdals3415@gmail.com>
2020-09-23 20:58:15 +0900

    Add edit function

--- site Root/routes/admin_pages.js
+++ site Root/routes/admin_pages.js
@@ -95,5 +95,73 @@ router.post('/reorder-pages',function(req,res){
     }
 });
 
+
+//get add page
+router.get('/edit-page/:slug',function(req,res){
+    Page.findOne({slug:req.params.slug},function(err, page){
+        if(err) return console.log(err);
+
+        res.render('admin/edit_page',{
+            title : page.title,
+            slug : page.slug,
+            content : page.content,
+            id: page._id
+        });
+    })
+    
+});
+
+//post edit page
+router.post('/edit-page/:slug',function(req,res){
+
+    req.checkBody('title','Title must have a value.').notEmpty();
+    req.checkBody('content','Content must have a value.').notEmpty();
+
+    let title = req.body.title;
+    let slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
+    if(slug == "") {
+        slug = title.replace(/\s+/g, '-').toLowerCase();
+    }
+    let content = req.body.content;
+    let id = req.body.id;
+    let errors = req.validationErrors();
+
+    if(errors){
+        res.render('admin/edit_page',{
+            errors : errors,
+            title : title,
+            slug : slug,
+            content : content,
+            id : id
+        });
+    }else{
+        Page.findOne({slug : slug, _id:{'$ne':id}}, function(err,page){
+            if(page){
+                req.flash('danger','Page slug exists, choose another');
+                res.render('admin/edit-page',{
+                    title : title,
+                    slug : slug,
+                    content : content,
+                    id : id
+                });
+
+            } else {
+                Page.findById(id, function(err, page){
+                    if(err) return console.log(err);
+                    page.title = title;
+                    page.slug = slug;
+                    page.content = content;
+                    page.save(function(err){
+                        if(err) return console.log(err);
+                        req.flash('success','Page added!');
+                        res.redirect('/admin/pages/edit-page/'+page.slug);   
+                    });
+                })
+               
+            }
+        })
+    }
+});
+
 //Exports
 module.exports = router;
