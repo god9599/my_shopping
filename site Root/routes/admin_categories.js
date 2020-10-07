3b9ed4527ce942de6b422f0652c28c46e14061fb
god9599 <59276134+god9599@users.noreply.github.com>
2020-10-07 23:05:13 +0900

    add function delete category

--- site Root/routes/admin_categories.js
+++ site Root/routes/admin_categories.js
@@ -120,12 +120,12 @@ router.post('/edit-category/:id',function(req,res){
 });
 
 
-//get delete page
-router.get('/delete-page/:id',function(req,res){
-    Page.findByIdAndRemove(req.params.id,function(err){
+//get delete category
+router.get('/delete-category/:id',function(req,res){
+    Category.findByIdAndRemove(req.params.id,function(err){
         if(err) return console.log(err);
-        req.flash('success','Page deleted!');
-        res.redirect('/admin/pages/');
+        req.flash('success','Category deleted!');
+        res.redirect('/admin/categories/');
     })
 })
 
